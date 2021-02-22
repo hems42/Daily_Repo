3d9d1dd3afcee32ad165148404cdd52315be25a5
caglar <eflatun126@gmail.com>
2021-02-23 01:14:13 +0300

    tamamlanan ve tamamlanmamış randevülerin sınıflarını ekleyip extend ettim

---
+++ app/src/main/java/Fragments/RandevuListesi/frg_TamamlanmisRandevuler.java
@@ -0,0 +1,10 @@
+package Fragments.RandevuListesi;
+
+import Patient.VisitInformations;
+import android.content.Context;
+
+public class frg_TamamlanmisRandevuler extends  Base_frg_RandevuListesi{
+    public frg_TamamlanmisRandevuler(Context context) {
+        super(context, VisitInformations.TAMAMLANDI);
+    }
+}
